588f39dd661d3ca5aec5986f2e9a02b7438c887b
sowmyapulimamidi <sowmyareddy3003@gmail.com>
2015-08-11 23:47:06 -0400

    Adding new file1

---
+++ NewApp.java
@@ -0,0 +1,20 @@
+package org.reddy.spring;
+
+//sowmya is a good girl,chaitanya is a bad boy///
+//sravan is a innocent boy :)///
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle=(Triangle) factory.getBean("triangle");
+		triangle.draw();
+
+	}
+
+}
